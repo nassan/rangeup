afba2068acf9701e1228dc264592c30fa97c2265
David <cargatser@gmail.com>
2017-04-04 15:30:19 -0700

    initial commit of code

---
+++ RangeUpApp/RangeUpApp/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace RangeUpApp
+{
+	public partial class App : Application
+	{
+        public static string IsRememberMePressedConst = "IsRememberMePressed";
+
+
+        public App ()
+		{
+			InitializeComponent();
+
+            // Whether pressed 'Rememeber me'
+            if (!IsRememberMePressed()) {
+                // Need to sign in
+                MainPage = new NavigationPage(new SignInPage());
+            }
+            // else {
+                // Need to go directly to the proper screen
+                // for Master - master screen
+                // for Field - filed screen
+            //}
+			//MainPage = new RangeUpApp.MainPage();
+		}
+
+        private bool IsRememberMePressed()
+        {
+            if (Application.Current.Properties.ContainsKey(IsRememberMePressedConst))
+            {
+                return (bool) Application.Current.Properties[IsRememberMePressedConst];
+            }
+            return false;
+        }
+
+        protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+
+}
---
+++ RangeUpApp/RangeUpApp/SignInPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RangeUpApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SignInPage : ContentPage
+	{
+
+        private bool isRememberMe = false;
+
+		public SignInPage(){
+			InitializeComponent ();
+		}
+
+        async void OnSignInbuttonClicked(object sender, EventArgs e)
+        {
+            var user = new User
+            {
+                Username = usernameEntry.Text,
+                Password = passwordEntry.Text
+            };
+
+            var isValid = AreCredentialsCorrect(user);
+
+            if (isValid)
+            {
+                if (isRememberMe){
+                    saveRememberMeChoice();
+                }
+                Navigation.InsertPageBefore(new MainPage(), this);
+                await Navigation.PopAsync();
+            } else
+            {
+                messageLabel.Text = "Login failed";
+                passwordEntry.Text = string.Empty;
+            }
+
+        }
+
+        private void saveRememberMeChoice()
+        {
+            Application.Current.Properties[App.IsRememberMePressedConst] = isRememberMe;
+        }
+
+        private bool AreCredentialsCorrect(User user)
+        {
+            // If findInDb(user.Username, user.Password) == true
+            // return true; else false;
+            // need to send data synch(!) to server in json
+            return true;
+
+        }
+        
+        private void OnSwitchToggle(object sender, ToggledEventArgs e)
+        {
+            if (e.Value)
+            {
+                isRememberMe = true;
+            }
+            else
+            {
+                isRememberMe = false;
+            }
+        }
+    }
+}
